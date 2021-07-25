7fde3f049f68b4b23a76e2b66ff5d0e0aa31e000
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-07-25 14:33:22 +0300

    animated overlay, hover and toggle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,97 @@
+// Toggle effect
+
 $(`.design-img`).click( () => {
-    $(`.design-img`).toggle();
-    $(`.p-design`).toggle();
+    $(`.design-img`).toggle(800);
+    $(`.p-design`).toggle(800);
 });
 
 $(`.p-design`).click( () => {
-    $(`.design-img`).toggle();
-    $(`.p-design`).toggle();
+    $(`.design-img`).toggle(800);
+    $(`.p-design`).toggle(800);
 });
 
 $(`.development-img`).click( () => {
-    $(`.development-img`).toggle();
-    $(`.p-development`).toggle();
+    $(`.development-img`).toggle(800);
+    $(`.p-development`).toggle(800);
 });
 
 $(`.p-development`).click( () => {
-    $(`.development-img`).toggle();
-    $(`.p-development`).toggle();
+    $(`.development-img`).toggle(800);
+    $(`.p-development`).toggle(800);
 });
 
 $(`.management-img`).click( () => {
-    $(`.management-img`).toggle();
-    $(`.p-management`).toggle();
+    $(`.management-img`).toggle(800);
+    $(`.p-management`).toggle(800);
 });
 
 $(`.p-management`).click( () => {
-    $(`.management-img`).toggle();
-    $(`.p-management`).toggle();    
+    $(`.management-img`).toggle(800);
+    $(`.p-management`).toggle(800);    
 });
+
+// Hover effect
+
+$(`.portfolio1`).hover( () => {
+    $(`.overlay1`).show(800);
+},
+    () => {
+    $(`.overlay1`).hide(800);
+    }
+)
+
+$(`.portfolio2`).hover( () => {
+    $(`.overlay2`).show(800);
+},
+    () => {
+    $(`.overlay2`).hide(800);
+    }
+)
+
+$(`.portfolio3`).hover( () => {
+    $(`.overlay3`).show(800);
+},
+    () => {
+    $(`.overlay3`).hide(800);
+    }
+)
+
+$(`.portfolio4`).hover( () => {
+    $(`.overlay4`).show(800);
+},
+    () => {
+    $(`.overlay4`).hide(800);
+    }
+)
+
+$(`.portfolio5`).hover( () => {
+    $(`.overlay5`).show(800);
+},
+    () => {
+    $(`.overlay5`).hide(800);
+    }
+)
+
+$(`.portfolio6`).hover( () => {
+    $(`.overlay6`).show(800);
+},
+    () => {
+    $(`.overlay6`).hide(800);
+    }
+)
+
+$(`.portfolio7`).hover( () => {
+    $(`.overlay7`).show(800);
+},
+    () => {
+    $(`.overlay7`).hide(800);
+    }
+)
+
+$(`.portfolio8`).hover( () => {
+    $(`.overlay8`).show(800);
+},
+    () => {
+    $(`.overlay8`).hide(800);
+    }
+)
