cdce67ee0d30790f5c9d2837abb5ffff1e912282
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-07-25 11:47:44 +0300

    added app.js and toggle effect on what we do section

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+$(`.design-img`).click( () => {
+    $(`.design-img`).toggle();
+    $(`.p-design`).toggle();
+});
+
+$(`.p-design`).click( () => {
+    $(`.design-img`).toggle();
+    $(`.p-design`).toggle();
+});
+
+$(`.development-img`).click( () => {
+    $(`.development-img`).toggle();
+    $(`.p-development`).toggle();
+});
+
+$(`.p-development`).click( () => {
+    $(`.development-img`).toggle();
+    $(`.p-development`).toggle();
+});
+
+$(`.management-img`).click( () => {
+    $(`.management-img`).toggle();
+    $(`.p-management`).toggle();
+});
+
+$(`.p-management`).click( () => {
+    $(`.management-img`).toggle();
+    $(`.p-management`).toggle();    
+});
